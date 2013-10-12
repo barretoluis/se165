84f18e0f154c9d0cdd559547ff091db335a833c4
Jerry P. <jerry.phul@gmail.com>
2013-10-11 18:05:09 -0700

    - Files already implemented in another file

--- code/auth/doLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once 'includes/user.php';
-    $userObj = new user();
-    session_start();
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $userObj->logInUser($username, $password);
-?>
