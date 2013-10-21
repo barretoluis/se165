acce80f4f228bbf1e7bbb44576c0b8c3dcd74d23
Jerry P. <jerry.phul@gmail.com>
2013-10-20 19:24:30 -0700

    - Updated code to remove hard coded values - Using constants instead now

--- code/search/autocomplete.php
+++ code/search/autocomplete.php
@@ -1,13 +1,16 @@
 <?php
+//TODO: This PHP page needs to implement the template. To be fixed later. Ask Jerry if any questions.
+require_once 'Configs/defineDb.php';
+
 // if the 'term' variable is not sent with the request, exit
 if ( !isset($_REQUEST['term']) )
 	exit;
- 
-$dblink = mysql_connect('localhost', 'tackster', '4tackster2use') or die( mysql_error() );
-mysql_select_db('db_tackster');
- 
+
+$dblink = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die( mysql_error() );
+mysql_select_db(DB_NAME);
+
 $rs = mysql_query('SELECT keyword from lkup_keyword where keyword like "'. mysql_real_escape_string($_REQUEST['term']) .'%" order by keyword asc limit 0,10', $dblink);
- 
+
 // loop through each keyword returned and format the response for jQuery
 $data = array();
 if ( $rs && mysql_num_rows($rs) )
@@ -20,7 +23,7 @@ if ( $rs && mysql_num_rows($rs) )
 		);
 	}
 }
- 
+
 // jQuery wants JSON data
 echo json_encode($data);
 flush();
