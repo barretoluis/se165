6f2ddf7fb438c179f67a2aa3b607463e06d62933
Jerry P. <jerry.phul@gmail.com>
2013-11-12 17:40:41 -0800

    - Place text for about and terms pages.

--- code/company/terms_and_privacy/index.php
+++ code/company/terms_and_privacy/index.php
@@ -33,7 +33,6 @@ try {
  * Page specific PHP code here
  */
 $ignorePageLogin = TRUE;
-
 ?><!DOCTYPE html>
 <html>
 	<head>
@@ -66,25 +65,94 @@ $ignorePageLogin = TRUE;
 
 
     <body>
-    <!-- Navigation Bar -->
-        <?php require_once('html/navbar.php'); ?>
-    <!-- /Navigation Bar -->
+		<!-- Navigation Bar -->
+		<?php require_once('html/navbar.php'); ?>
+		<!-- /Navigation Bar -->
 
-<!-- Body Content-->
-    <div id="terms" class="main" >
+		<!-- Body Content-->
+		<div name="terms" class="main" >
             <h3>Terms & Privacy</h3>
+			<ul>
+				<li><a href="#terms">Terms & Condition of Use</a></li>
+				<li><a href="#privacy">Privacy</a></li>
+			</ul>
+			<hr>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in lacus ligula. Donec consectetur tortor id felis ultricies mattis. Etiam ipsum nulla, sagittis et sollicitudin vitae, fermentum nec sem. Aliquam erat volutpat. Ut dui erat, suscipit eget ultrices non, tristique ac lectus. Etiam laoreet, est et elementum porttitor, felis lorem elementum diam, sed fringilla tortor augue vitae ante. Duis tincidunt nunc tellus, nec convallis nisi egestas a. Ut gravida, velit a semper dignissim, sem leo tempus tellus, quis blandit tellus massa vitae lectus. Maecenas quam sapien, lobortis nec dictum vitae, ultrices id magna. Pellentesque at nunc et odio egestas blandit vitae at elit. Vivamus egestas scelerisque vehicula. Etiam elementum, sapien quis sollicitudin ullamcorper, justo nunc dignissim urna, eu tempus ligula orci vitae enim. In hac habitasse platea dictumst. Ut et odio nec elit ornare posuere in eu lacus.</p>
+			<a id="terms"></a>
+			<h4>Web Site Terms and Conditions of Use</h4>
 
-            <p>Donec id tellus vel nunc condimentum vulputate a in sem. Donec et nisi suscipit, dignissim sapien id, varius turpis. Donec sagittis egestas sem rhoncus cursus. Duis consequat ligula vel sem vestibulum, in facilisis elit tincidunt. Ut scelerisque dapibus ipsum, a molestie lacus varius vitae. Praesent pulvinar id dui ac consectetur. Etiam dignissim posuere mi ut vulputate. Maecenas auctor nibh vitae tellus tempor, nec ultrices lectus porttitor. Phasellus dolor libero, dignissim eget tempor et, interdum vitae ligula.</p>
+			<h5>1. Terms</h5>
 
-            <p>Nulla volutpat erat eget urna mattis, ac vehicula ligula luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sed enim quis mi blandit vestibulum eu quis magna. Morbi blandit feugiat neque vitae vulputate. Maecenas eleifend pretium nunc dictum gravida. Nam feugiat sit amet augue eu auctor. Nullam vulputate nisl lacus, quis tincidunt eros semper nec.</p>
+			<p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</p>
 
-    </div>
-    <!-- /Body Content-->
+			<h5>2. Use License</h5>
 
-    <!-- Footer Content -->
-    <?php require_once('html/footer.php'); ?>
-    <!-- /Footer Content -->
+			<ol type="a">
+				<li>
+					Permission is granted to temporarily download one copy of the materials (information or software) on Tackster's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+
+					<ol type="i">
+						<li>modify or copy the materials;</li>
+						<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+						<li>attempt to decompile or reverse engineer any software contained on Tackster's web site;</li>
+						<li>remove any copyright or other proprietary notations from the materials; or</li>
+						<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+					</ol>
+				</li>
+				<li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Tackster at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
+			</ol>
+
+			<h5>3. Disclaimer</h5>
+
+			<ol type="a">
+				<li>The materials on Tackster's web site are provided "as is". Tackster makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Tackster does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</li>
+			</ol>
+
+			<h5>4. Limitations</h5>
+
+			<p>In no event shall Tackster or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Tackster's Internet site, even if Tackster or a Tackster authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+
+			<h5>5. Revisions and Errata</h5>
+
+			<p>The materials appearing on Tackster's web site could include technical, typographical, or photographic errors. Tackster does not warrant that any of the materials on its web site are accurate, complete, or current. Tackster may make changes to the materials contained on its web site at any time without notice. Tackster does not, however, make any commitment to update the materials.</p>
+
+			<h5>6. Links</h5>
+
+			<p>Tackster has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Tackster of the site. Use of any such linked web site is at the user's own risk.</p>
+
+			<h5>7. Site Terms of Use Modifications</h5>
+
+			<p>Tackster may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</p>
+
+			<h5>8. Governing Law</h5>
+
+			<p>Any claim relating to Tackster's web site shall be governed by the laws of the State of California without regard to its conflict of law provisions.</p>
+
+			<p>General Terms and Conditions applicable to Use of a Web Site.</p>
+
+			<p><br></p>
+			<a id="privacy"></a>
+			<h4>Privacy Policy</h4>
+
+			<p>Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.</p>
+
+			<ul>
+				<li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
+				<li>We will collect and use of personal information solely with the objective of fulfilling those purposes specified by us and for other compatible purposes, unless we obtain the consent of the individual concerned or as required by law.</li>
+				<li>We will only retain personal information as long as necessary for the fulfillment of those purposes.</li>
+				<li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
+				<li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+				<li>We will protect personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
+				<li>We will make readily available to customers information about our policies and practices relating to the management of personal information.</li>
+			</ul>
+
+			<p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</p>
+
+		</div>
+		<!-- /Body Content-->
+
+		<!-- Footer Content -->
+		<?php require_once('html/footer.php'); ?>
+		<!-- /Footer Content -->
     </body>
 </html>
