8075d0c0d61e54635f7879ca35c4613e0dcc8250
Jerry P. <jerry.phul@gmail.com>
2013-11-24 12:06:55 -0800

    Adjust nav menu, spacing, urls to better represent My Tracks and Following Tracks.

--- code/includes/html/navbar.php
+++ code/includes/html/navbar.php
@@ -80,9 +80,9 @@ if ($loggedIn) {
 						<li class="dropdown">
 							<a href="/" class="dropdown-toggle" data-toggle="dropdown" style="color: #00B800; font-size: 20px;">Tackster <b class="caret"></b></a>
 							<ul class="dropdown-menu">
-								<li><a href="/dashboard/">My Dashboard</a></li>
-								<li><a href="">Following Tracks</a></li>
-								<li><a id="simple-menu" href="#sidr">Tracks</a></li>
+								<li><a href="/dashboard/">Dashboard</a></li>
+								<li style="padding-left: 10px;"><a id="simple-menu" href="#sidr">My Tracks</a></li>
+								<li style="padding-left: 10px;"><a href="/dashboard/#following">Following Tracks</a></li>
 								<li class="divider"></li>
 								<li><a class='track_popUp' href="/track/createTrack.php">Add Tracks</a></li>
 								<li><a class='bookmark_popUp' href="/bookmark/createBookmark.php">Add Bookmark</a></li>
