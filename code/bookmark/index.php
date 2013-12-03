1498b98dde5ccf8efb383800b77ee4f2db85aba4
Miguel Gonzalez <mgonzalez@miguels-mbp.gateway.2wire.net>
2013-12-02 22:28:54 -0800

    changes

--- code/bookmark/index.php
+++ code/bookmark/index.php
@@ -210,7 +210,7 @@ if (count($_websiteErr) >= 1) {
                                         <td><?php if(!$isLiked) { ?><a href="/bookmark/likeBookmark.php/?bid=<?php echo $bmkId; ?>" title="Like Bookmark"><i class="fa fa-heart fa-lg"></i></a><?php } 
                                         else { ?> <a href="/bookmark/dislikeBookmark.php/?bid=<?php echo $bmkId; ?>" title="Like Bookmark"><i class="fa fa-heart fa-lg" style="opacity:0.2;"></i></a> <?php } ?>
                                         </td>
-                                        <td><a href="/track/followTrack.php/?ucid=<?php echo $ucId;?>&tid=<?php echo $bmkTid;?>" title="Follow Track"><i class="fa fa-eye fa-lg"></i></a></td>
+                                        <!--<td><a href="/track/followTrack.php/?ucid=<?php echo $ucId;?>&tid=<?php echo $bmkTid;?>" title="Follow Track"><i class="fa fa-eye fa-lg"></i></a></td> -->
                                     </tr>
                                     </table>
                                    
