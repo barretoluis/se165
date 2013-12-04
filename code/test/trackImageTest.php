9eea5f399f29911ec4f8deaa1f2833ecff9f06eb
Luis <barretoluis@gmail.com>
2013-12-04 11:12:21 -0800

    follow track controller

---
+++ code/test/trackImageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../includes/Track/Track.class.php';
+
+$trackObj = new Track();
+
+echo $trackObj->returnDefaultImage(3);
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
